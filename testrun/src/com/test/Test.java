2bf5c7f346cc3554e5ccb0f82a0d55d10228c997
WayneJr <blairu92@gmail.com>
2021-12-05 09:18:19 +0100

    moved module tips to Test

--- testrun/src/com/test/Test.java
+++ testrun/src/com/test/Test.java
@@ -8,3 +8,15 @@ public class Test {
         helloWorld.sayHello();
     }
 }
+
+// Principles of a good module.
+// 1. Strong Encapsulation - You should be able to properly expose
+// only needed interactions with the user or method that called the module.
+
+// 2. Stable Abstraction - You should be able to create an interface
+// for public use, that enables changes made, to be easily viewed by the
+// public.
+
+// 3. Dependencies - You need to be able to declare or define the
+// dependencies for your application. Modules can depend on other modules
+// as well.
